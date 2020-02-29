9fcd40d331807600e2efd29c2b23601a2e8882b4
kmjeon <lordet01@gmail.com>
2020-02-29 10:25:07 +0000

    working but slow at PI4

--- run_people_counter.sh
+++ run_people_counter.sh
@@ -5,9 +5,9 @@ python3 people_counter_ssd.py \
 	--skip-frames 5 \
 	--uart_port "/dev/ttyS0" \
 	--uart_baud 9600 \
-	--input "example_01.mp4" \
+	--input "rtsp://admin:intflow3121@192.168.1.64:554/Streaming/Channels/102/" \
 	--output "" \
-	--screen 0
+	--screen 1
 	#--input ../sample4.mp4 \
 	#--input videos/example_01.mp4
 	#--output output/output_01.avi
