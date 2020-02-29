7eb1ecc322e19d1e02c42025f4319655ffbf1f3a
kmjeon <lordet01@gmail.com>
2020-02-29 12:15:08 +0000

    script update

--- run_people_counter.sh
+++ run_people_counter.sh
@@ -1,14 +1,17 @@
+
+sudo ifconfig eth0 192.168.1.2
+
 python3 people_counter_ssd.py \
 	--prototxt models/MobileNetSSD_deploy.prototxt \
 	--model models/MobileNetSSD_deploy.caffemodel \
 	--confidence 0.3 \
 	--skip-frames 5 \
 	--resize_frame 160 \
-	--uart_port "/dev/ttyS0" \
+	--uart_port "/dev/ttyUSB0" \
 	--uart_baud 9600 \
-	--input "rtsp://admin:intflow3121@192.168.1.64:554/Streaming/Channels/102/" \
+	--input example_01.mp4 \
 	--output "" \
-	--screen 1 
-	#--input ../sample4.mp4 \
-	#--input videos/example_01.mp4
+	--screen 0
+
+	#--input "rtsp://admin:intflow3121@192.168.1.64:554/Streaming/Channels/102/" \
 	#--output output/output_01.avi
